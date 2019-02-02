5119cd0342f005af7317257f4ae85d368f5bf4f8
ChrisPTheCoder <ppham13@my.centennialcollege.ca>
2019-02-02 11:39:27 -0500

    added support for contact list page

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// database setup
+let mongoose = require("mongoose");
+let DB = require("./config/db");
+
+// point Mongoose to the DB URI
+mongoose.connect(DB.URI);
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+mongoDB.once('open', ()=> {
+  console.log("Connected to MongoDB...");
+})
+
+// route setup
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+let contactRouter = require('./routes/contact');
 
 let app = express();
 
@@ -21,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/contact-list', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
